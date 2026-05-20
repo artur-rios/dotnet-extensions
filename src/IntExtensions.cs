dda5329d18da4dc77dac22898dfc0431bd30cb8c
Artur Rios <arturdev@duck.com>
2026-05-20 08:23:56 -0300

    feat: add extension methods to check if a int number is prime

---
+++ src/IntExtensions.cs
@@ -0,0 +1,103 @@
+﻿namespace ArturRios.Extensions;
+
+/// <summary>
+/// Provides extension methods for integer types.
+/// </summary>
+public static class IntExtensions
+{
+    /// <summary>
+    /// Determines whether the specified number is a prime number.
+    /// </summary>
+    /// <param name="number">The number to check.</param>
+    /// <returns>true if the number is prime; otherwise, false.</returns>
+    public static bool IsPrime(this int number) => IsPrimeCore(number);
+
+    /// <summary>
+    /// Determines whether the specified number is a prime number.
+    /// </summary>
+    /// <param name="number">The number to check.</param>
+    /// <returns>true if the number is prime; otherwise, false.</returns>
+    public static bool IsPrime(this long number) => IsPrimeCore(number);
+
+    /// <summary>
+    /// Determines whether the specified number is a prime number.
+    /// </summary>
+    /// <param name="number">The number to check.</param>
+    /// <returns>true if the number is prime; otherwise, false.</returns>
+    public static bool IsPrime(this short number) => IsPrimeCore(number);
+
+    /// <summary>
+    /// Determines whether the specified number is a prime number.
+    /// </summary>
+    /// <param name="number">The number to check.</param>
+    /// <returns>true if the number is prime; otherwise, false.</returns>
+    public static bool IsPrime(this byte number) => IsPrimeCore(number);
+
+    /// <summary>
+    /// Determines whether the specified number is a prime number.
+    /// </summary>
+    /// <param name="number">The number to check.</param>
+    /// <returns>true if the number is prime; otherwise, false.</returns>
+    public static bool IsPrime(this uint number) => IsPrimeCore(number);
+
+    /// <summary>
+    /// Determines whether the specified number is a prime number.
+    /// </summary>
+    /// <param name="number">The number to check.</param>
+    /// <returns>true if the number is prime; otherwise, false.</returns>
+    public static bool IsPrime(this ulong number) => IsPrimeCore(number);
+
+    /// <summary>
+    /// Determines whether the specified number is a prime number.
+    /// </summary>
+    /// <param name="number">The number to check.</param>
+    /// <returns>true if the number is prime; otherwise, false.</returns>
+    public static bool IsPrime(this ushort number) => IsPrimeCore(number);
+
+    /// <summary>
+    /// Determines whether the specified number is a prime number.
+    /// </summary>
+    /// <param name="number">The number to check.</param>
+    /// <returns>true if the number is prime; otherwise, false.</returns>
+    public static bool IsPrime(this sbyte number) => IsPrimeCore(number);
+
+    /// <summary>
+    /// Core logic for checking if a number is prime.
+    /// </summary>
+    private static bool IsPrimeCore<T>(T number) where T : IComparable<T>, IEquatable<T>
+    {
+        // Convert to long for comparison and arithmetic operations
+        var num = Convert.ToInt64(number);
+
+        // Numbers less than 2 are not prime
+        if (num < 2)
+        {
+            return false;
+        }
+
+        // 2 is prime
+        if (num == 2)
+        {
+            return true;
+        }
+
+        // Even numbers (except 2) are not prime
+        if (num % 2 == 0)
+        {
+            return false;
+        }
+
+        // Check odd divisors up to sqrt(num)
+        var sqrt = (long)Math.Sqrt(num);
+
+        for (long i = 3; i <= sqrt; i += 2)
+        {
+            if (num % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ tests/ArturRios.Extensions.Tests/IntExtensionsTests.cs
@@ -0,0 +1,57 @@
+﻿namespace ArturRios.Extensions.Tests;
+
+public class IntExtensionsTests
+{
+    [Theory]
+    [InlineData(-5, false)]
+    [InlineData(0, false)]
+    [InlineData(1, false)]
+    [InlineData(2, true)]
+    [InlineData(3, true)]
+    [InlineData(4, false)]
+    [InlineData(17, true)]
+    [InlineData(18, false)]
+    [InlineData(7919, true)]
+    public void GivenInt_WhenCheckingIsPrime_ThenReturnsExpectedResult(int input, bool expected) => Assert.Equal(expected, input.IsPrime());
+
+    [Theory]
+    [InlineData(2L, true)]
+    [InlineData(3L, true)]
+    [InlineData(4L, false)]
+    [InlineData(1000000007L, true)]
+    [InlineData(1000000008L, false)]
+    public void GivenLong_WhenCheckingIsPrime_ThenReturnsExpectedResult(long input, bool expected) => Assert.Equal(expected, input.IsPrime());
+
+    [Theory]
+    [InlineData((short)-3, false)]
+    [InlineData((short)2, true)]
+    [InlineData((short)4, false)]
+    public void GivenShort_WhenCheckingIsPrime_ThenReturnsExpectedResult(short input, bool expected) => Assert.Equal(expected, input.IsPrime());
+
+    [Theory]
+    [InlineData((byte)0, false)]
+    [InlineData((byte)2, true)]
+    [InlineData((byte)9, false)]
+    public void GivenByte_WhenCheckingIsPrime_ThenReturnsExpectedResult(byte input, bool expected) => Assert.Equal(expected, input.IsPrime());
+
+    [Theory]
+    [InlineData((sbyte)-7, false)]
+    [InlineData((sbyte)3, true)]
+    public void GivenSByte_WhenCheckingIsPrime_ThenReturnsExpectedResult(sbyte input, bool expected) => Assert.Equal(expected, input.IsPrime());
+
+    [Theory]
+    [InlineData((ushort)2, true)]
+    [InlineData((ushort)100, false)]
+    public void GivenUShort_WhenCheckingIsPrime_ThenReturnsExpectedResult(ushort input, bool expected) => Assert.Equal(expected, input.IsPrime());
+
+    [Theory]
+    [InlineData(65537u, true)]
+    [InlineData(65536u, false)]
+    public void GivenUInt_WhenCheckingIsPrime_ThenReturnsExpectedResult(uint input, bool expected) => Assert.Equal(expected, input.IsPrime());
+
+    [Theory]
+    [InlineData(1000000007UL, true)]
+    [InlineData(1000000008UL, false)]
+    public void GivenULong_WhenCheckingIsPrime_ThenReturnsExpectedResult(ulong input, bool expected) => Assert.Equal(expected, input.IsPrime());
+}
+
